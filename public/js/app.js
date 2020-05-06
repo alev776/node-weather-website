9f50b72b6f8895820c93826923ca495c5bd21c4f
Alex <alrv0157@gmail.com>
2020-05-05 21:46:33 -0400

    Remove the a tags so it doesnt bother

--- public/js/app.js
+++ public/js/app.js
@@ -24,7 +24,7 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent = data.error;
             } else {
                 messageOne.textContent = data.location;
-                messageTwo.textContent = data.forecast;
+                messageTwo.textContent = data.forec ast;
             }
         });
     });
